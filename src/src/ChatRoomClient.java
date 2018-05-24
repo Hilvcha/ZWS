501ebeb1f6ffb26e93d04bc5d1d3d88fb387a6c3
Hilvcha <micah_wang@163.com>
2018-05-24 13:19:35 +0800

    不能空消息

--- src/src/ChatRoomClient.java
+++ src/src/ChatRoomClient.java
@@ -24,9 +24,10 @@ public class ChatRoomClient {
         pWriter = new PrintWriter(socket.getOutputStream());
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         new LinkServerFrame();
     }
+
     //发送消息
     public void sendMessage(String str) {
         pWriter.println(str);
@@ -53,79 +54,86 @@ public class ChatRoomClient {
 }
 
 //LinkServerFrame 登录服务器窗体
-class LinkServerFrame extends JFrame{
+class LinkServerFrame extends JFrame {
     private JTextField JTip;
     private JTextField JTusername;
-    private void linkPerformed(){
+
+    private void linkPerformed() {
         //  客户端连接时文本框不为空
-        if(!JTip.getText().equals("")&&!JTusername.getText().equals("")){
+        if (!JTip.getText().equals("") && !JTusername.getText().equals("")) {
             dispose();
             //  销毁客户端窗体
-            System.out.println("正在注册："+JTusername.getText().trim()+"···");
-            ClientFrame clientFrame=new ClientFrame(JTip.getText().trim(),JTusername.getText().trim());
+            System.out.println("正在注册：" + JTusername.getText().trim() + "···");
+            ClientFrame clientFrame = new ClientFrame(JTip.getText().trim(), JTusername.getText().trim());
             clientFrame.setVisible(true);
-        }else{
-            JOptionPane.showMessageDialog(null,"文本框内容不能为空","warning",JOptionPane.WARNING_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(null, "文本框内容不能为空", "warning", JOptionPane.WARNING_MESSAGE);
         }
     }
-    LinkServerFrame(){
+
+    LinkServerFrame() {
         int windowWidth = getWidth(); // 获得窗口宽
         int windowHeight = getHeight(); // 获得窗口高
         Toolkit kit = Toolkit.getDefaultToolkit(); // 定义工具包
         Dimension screenSize = kit.getScreenSize(); // 获取屏幕的尺寸
         int screenWidth = screenSize.width; // 获取屏幕的宽
         int screenHeight = screenSize.height; // 获取屏幕的高
-        setLocation(screenWidth/2-windowWidth/2, screenHeight/2-windowHeight/2);// 设置窗口居中显示
+        setLocation(screenWidth / 2 - windowWidth / 2, screenHeight / 2 - windowHeight / 2);// 设置窗口居中显示
 
         setTitle("zws~");
-        Container c=getContentPane();
-        setLayout(new GridLayout(3,1,100,30));
-        JPanel JPip=new JPanel(new GridLayout(1,2));
-        JPanel JPUserName=new JPanel(new GridLayout(1,2));
+        Container c = getContentPane();
+        setLayout(new GridLayout(3, 1, 100, 30));
+        JPanel JPip = new JPanel(new GridLayout(1, 2));
+        JPanel JPUserName = new JPanel(new GridLayout(1, 2));
 
         //ip面板组件
-        JLabel JLip=new JLabel("服务器ip：",JLabel.CENTER);
-        JLip.setFont(new Font("黑体",Font.PLAIN,50));
-        JTip=new JTextField("localhost");
-        JTip.setFont(new Font("黑体",Font.PLAIN,50));
+        JLabel JLip = new JLabel("服务器ip：", JLabel.CENTER);
+        JLip.setFont(new Font("黑体", Font.PLAIN, 50));
+        JTip = new JTextField("localhost");
+        JTip.setFont(new Font("黑体", Font.PLAIN, 50));
         JTip.setColumns(21);
-        JPip.add(JLip); JPip.add(JTip);
+        JPip.add(JLip);
+        JPip.add(JTip);
         JTip.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
                 super.keyPressed(e);
-                char word=e.getKeyChar();
-                if(word=='\n'){
+                char word = e.getKeyChar();
+                if (word == '\n') {
                     linkPerformed();
                 }
             }
         });
         //username面板组件
-        JLabel JLusername=new JLabel("客户端name：",JLabel.CENTER);
-        JLusername.setFont(new Font("黑体",Font.PLAIN,50));
-        JTusername=new JTextField("小机灵鬼");
+        JLabel JLusername = new JLabel("客户端name：", JLabel.CENTER);
+        JLusername.setFont(new Font("黑体", Font.PLAIN, 50));
+        JTusername = new JTextField("小机灵鬼");
         JTusername.setColumns(21);
-        JTusername.setFont(new Font("黑体",Font.PLAIN,50));
-        JPUserName.add(JLusername); JPUserName.add(JTusername);
+        JTusername.setFont(new Font("黑体", Font.PLAIN, 50));
+        JPUserName.add(JLusername);
+        JPUserName.add(JTusername);
 
         //登录事件
-        JButton JBlogin=new JButton("连接");
-        JBlogin.setSize(100,61);
-        JBlogin.setFont(new Font("黑体",Font.PLAIN,50));
+        JButton JBlogin = new JButton("连接");
+        JBlogin.setSize(100, 61);
+        JBlogin.setFont(new Font("黑体", Font.PLAIN, 50));
         JBlogin.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 linkPerformed();
             }
         });
-        c.add(JPip); c.add(JPUserName); c.add(JBlogin);
-        setSize(700,432);
+        c.add(JPip);
+        c.add(JPUserName);
+        c.add(JBlogin);
+        setSize(700, 432);
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
     }
 }
-class ClientFrame extends JFrame{
+
+class ClientFrame extends JFrame {
     private JPanel contentPane;
     private JLabel lblUsername;
     private JTextField tfMessage;
@@ -133,63 +141,71 @@ class ClientFrame extends JFrame{
     private JTextArea textArea;
     private String userName;
     private ChatRoomClient client;
-    private void sendPerformed(){
-        Date date=new Date();
+
+    private void sendPerformed() {
+        Date date = new Date();
         SimpleDateFormat df = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
-        client.sendMessage("%START%:"+userName+"  "+df.format(date)+":\n"+ tfMessage.getText()+"%END%");
+        client.sendMessage("%START%:" + userName + "  " + df.format(date) + ":\n" + tfMessage.getText() + "%END%");
         tfMessage.setText("");
     }
 
-    ClientFrame(String ip,String userName){
-        setTitle(userName+"的聊天室");
-        Container c=getContentPane();
+    ClientFrame(String ip, String userName) {
+        setTitle(userName + "的聊天室");
+        Container c = getContentPane();
         setLayout(new BorderLayout());
-        this.userName=userName;
+        this.userName = userName;
 
         try {
-            client=new ChatRoomClient(ip,4560);
-            client.sendMessage("%NAME%:"+userName);
-        }catch (UnknownHostException el){
+            client = new ChatRoomClient(ip, 4560);
+            client.sendMessage("%NAME%:" + userName);
+        } catch (UnknownHostException el) {
             System.out.println("host 无法处理");
             el.printStackTrace();
-        }catch (IOException el){
+        } catch (IOException el) {
             el.printStackTrace();
         }
-        ReadMessageThread messageThread=new ReadMessageThread();
+        ReadMessageThread messageThread = new ReadMessageThread();
         messageThread.start();
 //        发送消息事件
 
-        btnSend=new JButton("发送");
+        btnSend = new JButton("发送");
 
         btnSend.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                sendPerformed();
+                if (tfMessage.getText().equals("")) {
+                } else {
+                    sendPerformed();
+                }
             }
         });
-        lblUsername=new JLabel(userName);
-        contentPane=new JPanel();
-        tfMessage=new JTextField();
+        lblUsername = new JLabel(userName);
+        contentPane = new JPanel();
+        tfMessage = new JTextField();
         tfMessage.setColumns(20);
-        textArea=new JTextArea();
+        textArea = new JTextArea();
         contentPane.add(lblUsername);
         contentPane.add(tfMessage);
         contentPane.add(btnSend);
         tfMessage.addKeyListener(new KeyAdapter() {
             char word;
+
             @Override
             public void keyPressed(KeyEvent e) {
                 super.keyTyped(e);
-                word=e.getKeyChar();
-                if(word=='\n'){
-                    sendPerformed();
+                word = e.getKeyChar();
+                if (word == '\n') {
+                    if (tfMessage.getText().equals("")) {
+                    } else {
+                        sendPerformed();
+                    }
                 }
             }
         });
-        JScrollPane talkwindow=new JScrollPane(textArea);
-        c.add(talkwindow,BorderLayout.CENTER);
-        c.add(contentPane,BorderLayout.SOUTH);
-        setSize(600,370);
+        JScrollPane talkwindow = new JScrollPane(textArea);
+        c.add(talkwindow, BorderLayout.CENTER);
+        c.add(contentPane, BorderLayout.SOUTH);
+        setSize(600, 370);
         setVisible(true);
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         setLocationRelativeTo(null);
@@ -207,12 +223,12 @@ class ClientFrame extends JFrame{
             @Override
             public void windowClosing(WindowEvent atg0) {
                 super.windowClosing(atg0);
-                int op=JOptionPane.showConfirmDialog(ClientFrame.this,"确定要退出聊天室吗~","确定",JOptionPane.YES_NO_OPTION);
-                if(op==JOptionPane.YES_OPTION){
-                    client.sendMessage("%EXIT%:"+userName);
-                    try{
+                int op = JOptionPane.showConfirmDialog(ClientFrame.this, "确定要退出聊天室吗~", "确定", JOptionPane.YES_NO_OPTION);
+                if (op == JOptionPane.YES_OPTION) {
+                    client.sendMessage("%EXIT%:" + userName);
+                    try {
                         Thread.sleep(200);
-                    }catch (InterruptedException e){
+                    } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                     client.close();
@@ -222,36 +238,37 @@ class ClientFrame extends JFrame{
         });
 
     }
-//负责不断解析收到的一行一行的信息
-    private class ReadMessageThread extends Thread{
-        public void run(){
-            while(true){
-                String str=client.reciveMessage();
-                System.out.println("接收到消息:"+str);
-                if (str.contains("%NAMEERROR%")){
-                    JOptionPane.showMessageDialog(ClientFrame.this,"你的名字重复了~");
+
+    //负责不断解析收到的一行一行的信息
+    private class ReadMessageThread extends Thread {
+        public void run() {
+            while (true) {
+                String str = client.reciveMessage();
+                System.out.println("接收到消息:" + str);
+                if (str.contains("%NAMEERROR%")) {
+                    JOptionPane.showMessageDialog(ClientFrame.this,"你的名字重复了~","warning",JOptionPane.WARNING_MESSAGE);
                     ClientFrame.this.dispose();
                     new LinkServerFrame();
                 }
-                if(str.contains("%START%")){
+                if (str.contains("%START%")) {
 //                    System.out.println("将要打印文本："+str.split(":")[0]);
-                    if(str.contains("%END%")){//开始结束在同一行
-                        textArea.append(str.replaceAll("%START%",""));
-                        textArea.append(str.replaceAll("%END%",""));
-                        textArea.append(str+"\n");
+                    if (str.contains("%END%")) {//开始结束在同一行
+                        textArea.append(str.replaceAll("%START%", ""));
+                        textArea.append(str.replaceAll("%END%", ""));
+                        textArea.append(str + "\n");
                         textArea.setSelectionStart(textArea.getText().length());
                         continue;
                     }
-                    textArea.append(str.split(":",2)[1]+"\n");
+                    textArea.append(str.split(":", 2)[1] + "\n");
                     textArea.setSelectionStart(textArea.getText().length());
 
-                    while(true){
-                        str=client.reciveMessage();
-                        if(str.contains("%END%")){
-                            textArea.append(str.replaceAll("%END%","")+"\n");
+                    while (true) {
+                        str = client.reciveMessage();
+                        if (str.contains("%END%")) {
+                            textArea.append(str.replaceAll("%END%", "") + "\n");
                             break;
                         }
-                        textArea.append(str+"\n");
+                        textArea.append(str + "\n");
                         textArea.setSelectionStart(textArea.getText().length());
 
                     }
--- src/src/ChatRoomServer.java
+++ src/src/ChatRoomServer.java
@@ -11,25 +11,28 @@ import java.util.HashSet;
 
 public class ChatRoomServer {
     private ServerSocket serverSocket;
-    private HashMap<Socket,String> alluser;
-//    聊天室服务器构造方法
-    public ChatRoomServer(){
+    private HashMap<Socket, String> alluser;
+
+    //    聊天室服务器构造方法
+    public ChatRoomServer() {
         try {
-            serverSocket=new ServerSocket(4560);
-        }catch (IOException e){
+            serverSocket = new ServerSocket(4560);
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        alluser=new HashMap<>();
+        alluser = new HashMap<>();
     }
-//    启动服务器
-    public void startService() throws IOException{
+
+    //    启动服务器
+    public void startService() throws IOException {
         System.out.println("服务器已启动，等待用户加入···");
-        while  (true){
-            Socket s= serverSocket.accept();
+        while (true) {
+            Socket s = serverSocket.accept();
             new ServerThread(s).start();
         }
     }
-//    服务线程内部类
+
+    //    服务线程内部类
     private class ServerThread extends Thread {
         Socket socket;
 
@@ -43,14 +46,14 @@ public class ChatRoomServer {
                 br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 while (true) {
                     String str = br.readLine();
-                    if(str.contains("%NAME%")){
-                        String name=str.split(":")[1];
+                    if (str.contains("%NAME%")) {
+                        String name = str.split(":")[1];
                         //用户名有重复
-                        if(alluser.values().contains(name)){
-                            String message="%NAMEERROR%";
-                            sendMessageToONEClient(message,socket);
-                        }else {
-                            alluser.put(socket,name);
+                        if (alluser.values().contains(name)) {
+                            String message = "%NAMEERROR%";
+                            sendMessageToONEClient(message, socket);
+                        } else {
+                            alluser.put(socket, name);
                         }
                         continue;
                     }
@@ -67,25 +70,28 @@ public class ChatRoomServer {
                 e.printStackTrace();
             }
         }
-        public void sendMessageToONEClient(String message,Socket s) throws IOException{
-            System.out.println("将要向"+s+"发送:"+message);
-            PrintWriter pw=new PrintWriter(s.getOutputStream());
+
+        public void sendMessageToONEClient(String message, Socket s) throws IOException {
+            System.out.println("将要向" + s + "发送:" + message);
+            PrintWriter pw = new PrintWriter(s.getOutputStream());
             pw.println(message);
             pw.flush();
         }
-        public void sendMessageTOAllClient(String message) throws IOException{
-            System.out.println("将要向所有用户发送:"+message);
-                for(Socket s:alluser.keySet()){
-                    PrintWriter pw=new PrintWriter(s.getOutputStream());
-                    pw.println(message);
-                    pw.flush();
-                }
+
+        public void sendMessageTOAllClient(String message) throws IOException {
+            System.out.println("将要向所有用户发送:" + message);
+            for (Socket s : alluser.keySet()) {
+                PrintWriter pw = new PrintWriter(s.getOutputStream());
+                pw.println(message);
+                pw.flush();
+            }
         }
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         try {
             new ChatRoomServer().startService();
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
